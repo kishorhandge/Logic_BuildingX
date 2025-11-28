26b78d6897971e46e528b316383414383c9f35be
kishorhandge <kishorhandge29@gmail.com>
2025-11-28 11:46:19 +0530

    Add files via upload

---
+++ program219.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int Count(char str[])
+{   
+    int iCount = 0;
+
+    while(*str != '\0')
+    {   
+        if(*str =='A' )
+        {
+            iCount++;
+           
+        }
+        str++;
+       
+    } 
+    return iCount;  
+}
+
+int main()
+{   
+    char Arr[50] = {'\0'};
+    int iRet = 0;
+
+    printf("Enter String :\n");
+    scanf("%[^'\n']s",Arr);
+
+    iRet = Count(Arr);
+
+    printf("Frequency of A is : %d\n",iRet);
+
+    return 0;
+}
