a9a7dd8ba873ac40c1fba8e780f9d907427833e7
kishorhandge <kishorhandge29@gmail.com>
2025-12-01 23:53:48 +0530

    Update program57.c

--- program57.c
+++ program57.c
@@ -11,11 +11,6 @@ bool CheckPrime(int iNo)
         iNo=-iNo;
     }
     
-    if (iNo == 0 || iNo == 1)
-    {
-        return false; // 0 and 1 are NOT prime
-    }
-    
     for(iCnt=2;iCnt<=(iNo/2);iCnt++)
     {
         if((iNo%iCnt) == 0)
@@ -54,3 +49,4 @@ int main()
 //Time Complexity For Prime : O(N/2)
 
 //Time Complexity For Non Prime :Either 1 Or 2
+
