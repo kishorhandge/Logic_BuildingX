f72a3c32dfa9a112c8ddd8e7dad6c621edf33c3b
kishorhandge <kishorhandge29@gmail.com>
2026-01-13 23:04:38 +0530

    Add files via upload

---
+++ File_Operations/program505.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+//#include<unistd.h>
+#include<fcntl.h>
+
+int main()
+{   
+    int fd = 0;
+
+    fd = open("LB.txt",O_RDWR);     //O - means open flag
+
+    if(fd == -1)
+    {
+        printf("Unable to open a file:\n");
+    }
+    else
+    {
+        printf("file gets successfully open with fd :%d\n",fd);
+        write(fd,"Jay Ganesh",10);
+
+        close(fd);
+    }
+
+     
+    return 0;
+}
