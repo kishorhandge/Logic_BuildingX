a70b648992dc0861ad8e67908b6f1641f3fa3ed4
kishorhandge <kishorhandge29@gmail.com>
2026-01-30 23:41:26 +0530

    Add files via upload

---
+++ Packer_Unpacker_Incremental_Approach/program562.java
@@ -0,0 +1,24 @@
+import java.io.FileWriter;
+import java.util.Scanner;
+
+class program562
+{
+    public static void main(String A[]) throws Exception
+    {   
+        boolean bRet = false;
+        String FileName = null;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the name of file");
+        FileName = sobj.nextLine();
+
+        FileWriter fwobj = new FileWriter(FileName);
+
+        fwobj.write("Jay Ganesh...");
+
+        fwobj.close();
+        sobj.close();
+
+    }
+}
