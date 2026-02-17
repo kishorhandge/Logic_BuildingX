cf8783850b2fae984217cfacfc93e47aaec7bb7e
kishorhandge <kishorhandge29@gmail.com>
2026-02-17 23:34:42 +0530

    Add files via upload

---
+++ program632.java
@@ -0,0 +1,22 @@
+
+class program632
+{
+    public static void main(String A[])
+    {   
+         
+        int iNo = 1;
+        int iResult = 0;
+
+        iResult = iNo  << 8;     
+
+        System.out.println(iResult);
+    }
+    
+}
+/*
+
+    128*2 = 256<-128 64 32 16 8 4 2 1
+    00000001 00000000 -->1 is at 2⁸ position
+*/
+
+ 
---
+++ program634.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class program634
+{
+    public static void main(String A[])
+    {   
+         
+
+    }
+    
+}
+
+ 
