d25e8de497b41ef802f8c720356799a895a54669
kishorhandge <kishorhandge29@gmail.com>
2026-02-18 23:49:06 +0530

    Add files via upload

---
+++ program641.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void Display()
+{
+    cout<<"*\n";
+    cout<<"*\n";
+    cout<<"*\n";
+    cout<<"*\n";
+}
+int main()
+{
+    Display();
+
+    return 0;
+}
---
+++ program643.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void Display()
+{
+   int iCnt = 0;
+
+   iCnt = 1;
+
+   while(iCnt <= 4)
+   {
+        cout<<"*\n";
+        iCnt++;
+   }
+
+}
+int main()
+{
+    Display();
+
+    return 0;
+}
