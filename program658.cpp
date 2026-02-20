d722084c59c96747751244758ade2151ab6019ce
kishorhandge <kishorhandge29@gmail.com>
2026-02-20 23:29:17 +0530

    Add files via upload

---
+++ program658.cpp
@@ -0,0 +1,31 @@
+//input--> 4
+//output--> 1+2+3+4 = 10
+
+#include<iostream>
+using namespace std;
+
+int Addition(int iNo)
+{   
+    static int iSum = 0;
+    static int i = 1;
+    
+    if(i <= iNo)
+    {
+        iSum = iSum + i;
+        i++;
+        Addition(iNo);
+        
+    }
+    return iSum;
+
+}
+int main()
+{   
+    int iRet = 0;
+
+    iRet = Addition(4);
+
+    cout<<"Addition is :"<<iRet<<"\n";
+
+    return 0;
+}
