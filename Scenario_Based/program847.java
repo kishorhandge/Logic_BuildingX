481e48a39bc37fc96a4333adc053b4079cef26c4
kishorhandge <kishorhandge29@gmail.com>
2026-03-18 23:20:01 +0530

    Add files via upload

---
+++ Scenario_Based/program847.java
@@ -0,0 +1,17 @@
+
+class program847
+{   
+    
+    public static void main(String A[])
+    {   
+        String str = "  india   is   my  country  ";
+
+        System.out.println(str);
+        str = str.trim();
+
+        str = str.replaceAll("  ", " ");
+
+        System.out.println(str);
+
+    }
+}
