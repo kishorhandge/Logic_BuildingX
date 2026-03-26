e04e13938a83f6e8ab57a1082612e7fb240ee774
kishorhandge <kishorhandge29@gmail.com>
2026-03-26 22:57:00 +0530

    Add files via upload

---
+++ Scenario_Based/program885.java
@@ -0,0 +1,42 @@
+
+import java.util.*;
+
+class Student
+{
+    public String Name;
+    public int Marks;
+
+    public Student(String a , int b)
+    {
+        this.Name = a;
+        this.Marks = b;
+    }
+
+    public void Display()
+    {
+        System.out.println(Name + " : "+Marks);
+    }
+
+}
+
+class program885
+{   
+    public static void main(String A[])
+    {   
+        Scanner sobj = new Scanner(System.in);
+
+        ArrayList <Integer> aobj = new ArrayList <Integer> ();
+
+        Student s1 = new Student("Sagar", 90);
+        Student s2 = new Student("Rahul", 80);
+        Student s3 = new Student("Pooja", 92);
+        Student s4 = new Student("Amit",  72);
+        
+        System.out.println(s1);
+        System.out.println(s2);
+
+        s1.Display();
+        s2.Display();
+    }
+}
+
